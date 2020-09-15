41d2144e870e6bedeca9d9faf7e27ef8f8cdde4a
Daniel <daniel@lngzl.nl>
2020-09-15 12:01:17 +0200

    :sparkles: Postgres YES

--- sql/tests.js
+++ sql/tests.js
@@ -1,5 +1,6 @@
 module.exports = {
   create_postgres: 'CREATE TABLE test (userid SERIAL, name VARCHAR(100), lastname VARCHAR(100));',
   insert: `INSERT INTO test (name, lastname) VALUES ('${'a'.repeat(100)}', '${'b'.repeat(100)}');`,
-  find: 'SELECT * FROM test WHERE user = "1";'
+  find: 'SELECT * FROM test WHERE userid = 99;',
+  drop: 'DROP TABLE test;'
 }
--- tests/postgresql.js
+++ tests/postgresql.js
@@ -1,7 +1,8 @@
-
+const PreciseTimer = require('precise-timer')
 const tests = require('../sql/tests')
 
 const { Client } = require('pg')
+
 const client = new Client({
   user: 'postgres',
   host: 'localhost',
@@ -11,13 +12,41 @@ const client = new Client({
 })
 
 client.connect()
-client.query(tests.insert, (err, res) => {
+
+var results = {
+  create: 0,
+  insert: 0,
+  find: 0,
+  drop: 0
+}
+// Create the test table
+const timer = new PreciseTimer({ decimals: 6 })
+client.query(tests.create_postgres, (err, res) => {
+  if (err) console.error(err)
+  results.create = timer.elapsed
+})
+
+// 100 times the same query
+for (let i = 0; i < 100000; i++) {
+  client.query(tests.insert, (err, res) => {
+    if (err) console.error(err)
+    results.insert = timer.elapsed - results.create
+  })
+}
+
+// Find one thing
+client.query(tests.find, (err, res) => {
   console.log(err ? err.stack : res.rows)
-  client.end()
+  results.find = timer.elapsed - (results.insert + results.create)
+})
+
+// Drop the table
+client.query(tests.drop, (err, res) => {
+  if (err) console.error(err)
+  results.drop = timer.elapsed - (results.find + results.insert + results.create)
+  console.table(results)
 })
 
 module.export = {
-  result: {
-    write: '10ms'
-  }
+  results
 }
